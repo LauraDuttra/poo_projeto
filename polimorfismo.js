067536985529bdb45f6d9478aecdf80092d5337e
Laura Dutra <lauraduttra1@gmail.com>
2025-03-24 15:58:26 -0300

    Update polimorfismo.js

--- polimorfismo.js
+++ polimorfismo.js
@@ -1,30 +1,32 @@
-<?php
- class Pessoa {
-     public $nome;
-     public function __construct($nome)
-     {
-         $this->nome = $nome;
-     }
-     public fuNction apresentar(){
-         echo "Olá, sou {$this->nome}";
-     }
- }
- // A classe Aluno Herda de Pessoa e sobrescreve o método apresentar
- 
- class Aluno extends Pessoa{
-     public function apresentar(){
-         echo "Oi! Meu nome é {$this->nome} e sou aluno </br>";
-     }
- }
- class Professor extends Pessoa{
-     public function apresentar(){
-         echo "Oi! Eu sou o Professor {$this->nome} </br>";
-     }
- }
- $rayssa = new Aluno ("Rayssa", 19);
- $raul = new Professor("Raul", 36);
- 
- 
- echo $rayssa->apresentar();
- echo $raul->apresentar();
- ?>
+//  O polimorfismo permite que diferentes CLASSES tenhsm o mesmo método, mas 
+// funcionando de formas diferentes
+
+
+class pessoa{
+    constructor(nome) {
+        // o constructor é um método tradicional
+        this.nome = nome;
+    }
+    apresentar= () =>console.log (`Oi meu nome é, ${this.nome} e sou uma aluna.`)
+}
+
+class  Aluno extends pessoa{
+    apresetar = () =>console.log(`Oi meu nome é, ${this.nome} e sou um aluno.`);
+}
+
+class Professor extends pessoa{
+    apresentar = () =>console.log(`Oi meu nome é, ${this.nome} e sou um professor.`)
+}
+
+// criando objetos
+const Isabela = new Aluno ("Isabela");
+const Raul = new Professor ("Raul")
+
+Isabela.apresentar();
+Raul.apresentar();
+
+
+// Explicação Polimorfismo
+// - PESSOA tem um método padrão APRESENTAR();
+// - ALUNO e PROFESSOR herdam esse metodo, mas reescrevem, para exibir msg diferentes.
+// - Mesmo metodo, e comportamentos diferentes isso é polimorfismo.
